e458ff85d20103acf16b906fa46a45d050ffb2b5
burry <burry439@gmail.com>
2018-08-16 19:46:52 +0300

    fff

---
+++ index.js
@@ -0,0 +1,12 @@
+var express = require('express');
+var app = express();
+
+
+app.use(express.static('public'));
+app.use(express.static('node_modules'));
+
+
+
+app.listen(process.env.PORT || 8000, function() {
+    console.log("yo yo yo, on 8000!!")
+  })

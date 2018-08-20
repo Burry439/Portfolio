be424f2549657ff350abdb06857378b7f2add5b5
burry <burry439@gmail.com>
2018-08-20 12:43:02 +0300

    fixd popup modal

--- dist/js/main.js
+++ dist/js/main.js
@@ -55,32 +55,30 @@ modalText = [
 
 
 var modal = document.getElementById('myModal');
-
-
-
+ // Get the button that opens the modal
+// var btn = document.getElementById("myBtn");
+ // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
-
-
-var p = document.getElementById("modalText");
-
-
-
+ var p = document.getElementById("modalText");
+ // When the user clicks on the button, open the modal 
+// btn.onclick = function() {
+//     modal.style.display = "block";
+//     p.innerHTML = "yoyoyoy"
+// }
+ // When the user clicks on <span> (x), close the modal
 span.onclick = function() {
     modal.style.display = "none";
 }
-
+ // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
     if (event.target == modal) {
         modal.style.display = "none";
     }
 }
-
-openModal = (index)=>{
+ openModal = (index)=>{
     modal.style.display = "block";
     p.innerHTML = modalText[index]
-}
-
-
+} 
 
 ////////////image modal/////////////////////
 
